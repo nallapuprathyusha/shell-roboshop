9d942acd5e256222d9c6120f031ada5b1b0e5d88
prathyusha <prathyusha0612@gmail.com>
2025-09-26 13:04:34 +0530

    mongodb

---
+++ 2_mongodb.sh
@@ -0,0 +1,39 @@
+#!bin/bash
+
+set -e
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "your not root user please switch to root user"
+if
+
+
+CHECK(){
+    
+    if [ $? -ne 0 ]; then
+    echo "$2 is failure" 
+    else
+    echo "$? is success"
+}
+
+
+dnf list isntalled mongodb-org
+CHECK $? "Mongodb check"
+
+cp mongo.repo /etc/yum/respos.d/mongo.repo
+CHECK $? "copying mongo repo file to repository"
+
+dnf install mongodb-org -y
+CHECK $? "mongo installed"
+
+systemctl enable mongod
+VALIDATE $? "Enable MongoDB"
+
+
+systemctl start mongod 
+CHECK $? "mongo started"
+
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+systemctl daemon restart
+CHECK $? "mongo restarted"
