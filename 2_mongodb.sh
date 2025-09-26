b9526d4a21c5d7c7e21c0cb9f9aed578b39e92a6
prathyusha <prathyusha0612@gmail.com>
2025-09-26 15:27:31 +0530

    commit

--- 2_mongodb.sh
+++ 2_mongodb.sh
@@ -6,7 +6,7 @@ USERID=$(id -u)
 LOG_FLODER="etc/var/logs/shell-scripting"
 SCRIPT_NAME= echo($0 | cut -d "." -f)
 LOG_FILE_NAME=$LOG_FLODER/$SCRIPT_NAME.log
-echo  $LOG_FILE_NAME
+echo  "$LOG_FILE_NAME"
 
 
 if [ $USERID -ne 0 ]; then
