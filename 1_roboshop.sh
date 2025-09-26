8580a605c5ba0e996bcd17b170ff95cbb4b674f4
prathyusha <prathyusha0612@gmail.com>
2025-09-26 12:18:19 +0530

    creating instance and adding to records

--- 1_roboshop.sh
+++ 1_roboshop.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-029dab38d39f77779" # replace with your SG ID
+SG_ID="sg-029dab38d39f77779"  # replace with your SG ID
+DOMAIN="prathyusha.fun"  # replace with your domain name
+HOSTZONE="Z0994508312PR2YKTSFA1"
 
 for instance in $@
 do 
@@ -11,9 +13,31 @@ do
    if [ $instance != "frontend" ]; then
 
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+      RECORDNAME=$instance.$DOMAIN
    else
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text) 
+      RECORDNAME=$DOMAIN
+
       
    fi
 echo "$instance: $IP"
+
+   aws route53 change-resource-record-sets --hosted-zone-id $HOSTZONE --change-batch '{
+    "Comment": "Creating an A record for prathyusha.fun",
+    "Changes": [
+      {
+        "Action": "UPSERT",
+        "ResourceRecordSet": {
+          "Name": "'$RECORDNAME'",
+          "Type": "A",
+          "TTL": 1,
+          "ResourceRecords": [
+            {
+              "Value": "'$IP'"
+            }
+          ]
+        }
+      }
+    ]
+  }'
 done
