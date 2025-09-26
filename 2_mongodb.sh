cf84fbbf269c62167fa26c07de7146dfb60edaa4
prathyusha <prathyusha0612@gmail.com>
2025-09-26 15:42:29 +0530

    commit

--- 2_mongodb.sh
+++ 2_mongodb.sh
@@ -6,7 +6,9 @@ USERID=$(id -u)
 LOG_FLODER="/var/logs/shell-scripting"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f )
 
-LOG_FILE_NAME=$LOG_FLODER/$SCRIPT_NAME.log
+LOG_FILE=$LOG_FLODER/$SCRIPT_NAME.log
+
+mkdir -p  $LOG_FLODER
 echo  $LOG_FILE_NAME
 
 
