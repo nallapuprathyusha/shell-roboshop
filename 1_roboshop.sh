b581abb13540c84f569c2e1309e21dd92009646e
prathyusha <prathyusha0612@gmail.com>
2025-09-26 11:13:15 +0530

    creating instance and adding to records

---
+++ 1_roboshop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-029dab38d39f77779" # replace with your SG ID
+
+for instance in $@
+do 
+   INSTANCE_ID= $(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-029dab38d39f77779 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
+
+   
+
+
+done
