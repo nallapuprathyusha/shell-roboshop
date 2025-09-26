5179b5edcd08cf64f9b65964dde46dba25eaf2d8
prathyusha <prathyusha0612@gmail.com>
2025-09-26 11:50:37 +0530

    creating instance and adding to records

--- 1_roboshop.sh
+++ 1_roboshop.sh
@@ -15,7 +15,4 @@ do
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text) 
       
              # IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-
-
-
 done
