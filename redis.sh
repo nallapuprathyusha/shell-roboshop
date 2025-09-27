00bbbde576133310ddc55c312041d5db6c25c07a
prathyusha <prathyusha0612@gmail.com>
2025-09-27 13:42:36 +0530

    commit

--- redis.sh
+++ redis.sh
@@ -49,7 +49,14 @@ CHECK $? "enabling redis"
 dnf install redis -y  &>> $LOG_FILE
 CHECK $? "installing redis"
 
-sec -i 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+#sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+# sed -i -e 's/127.0.0.1/0.0.0.0 -e /protected-mode/c protected-mode no'/etc/redis/redis.conf &>>$log
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf &>>$log
+validate $? "Redis Enabling Public Access"
+
+sed -i '/protected-mode/c protected-mode no' /etc/redis/redis.conf &>>$log
+validate $? "Protected Mode off"
+
 
 systemctl enable redis  &>> $LOG_FILE
 CHECK $? "Enabling redis"
