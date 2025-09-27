6d971855cf0a383acc248c0e5d745b1d57389fe7
prathyusha <prathyusha0612@gmail.com>
2025-09-27 22:21:12 +0530

    commit

---
+++ mysql.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+LOG_FOLDER="/var/log/shell-scripting"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+
+LOG_FILE=$LOG_FOLDER/$SCRIPT_NAME.log
+
+mkdir -p  $LOG_FOLDER
+
+#echo  $LOG_FILE
+#tee -a <File_name> -  display output and stores in file
+#&>> -<file_name> dont display the output ,just stores the output
+
+
+if [ $USERID -ne 0 ]; then
+    echo "your not root user please switch to root user" | tee -a $LOG_FILE
+fi
+
+CHECK()
+{
+    
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf install mysql-server -y
+CHECK $? "installing mysql"
+
+systemctl enable mysqld
+CHECK $? "enabling mysqld"
+
+systemctl start mysqld
+CHECK $? "start mysqld" 
+
+mysql_secure_installation --set-root-pass RoboShop@1
+CHECK $? "setting password for mysql root user"
