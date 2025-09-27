3dc49fe1eee867531995db103eb91393a5ab21af
prathyusha <prathyusha0612@gmail.com>
2025-09-27 23:35:21 +0530

    commit

--- payment.sh
+++ payment.sh
@@ -35,40 +35,40 @@ CHECK()
     
 }
 
-dnf install python3 gcc python3-devel -y
+dnf install python3 gcc python3-devel -y   &>> $LOG_FILE
 CHECK $? "installing python"
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop   &>> $LOG_FILE
 CHECK $? "creating system user"
 
-mkdir /app 
+mkdir /app   &>> $LOG_FILE
 CHECK $? "creating app directory"
 
-rm -rf app/*
+rm -rf app/*   &>> $LOG_FILE
 CHECK $? "removing old files in directory"
 
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip 
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip    &>> $LOG_FILE
 CHECK $? "downloading application files"
 
-cd /app
+cd /app   &>> $LOG_FILE
 CHECK $? "changing to app directory" 
 
-unzip /tmp/payment.zip
+unzip /tmp/payment.zip   &>> $LOG_FILE
 CHECK $? "unziping the files"
 
 
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt   &>> $LOG_FILE
 CHECK $? "installing dependencies"
 
-cp /root/shell-roboshop/payment.service /etc/systemd/system/payment.service
+cp /root/shell-roboshop/payment.service /etc/systemd/system/payment.service   &>> $LOG_FILE
 CHECK $? "creating payment services"
 
 
-systemctl daemon-reload
+systemctl daemon-reload   &>> $LOG_FILE
 CHECK $? "daemon reloading"
 
-systemctl enable payment 
+systemctl enable payment    &>> $LOG_FILE
 CHECK $? "enabling payment"
 
-systemctl start payment
+systemctl start payment   &>> $LOG_FILE
 CHECK $? "starting payment"
